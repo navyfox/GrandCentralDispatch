76cbeed6c47c96492cf69b96a13b7975c6f2ea69
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-08-10 11:58:33 +0600

    Interface

---
+++ GrandCentralDispatch/PictureViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PictureViewController.swift
+//  GrandCentralDispatch
+//
+//  Created by Игорь Михайлович Ракитянский on 10.08.16.
+//  Copyright © 2016 RIM. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
