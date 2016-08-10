bd07ffcea78d02490acc86f4bfef692bf68feb02
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-08-10 11:41:57 +0600

    Create

---
+++ GrandCentralDispatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrandCentralDispatch
+//
+//  Created by Игорь Михайлович Ракитянский on 10.08.16.
+//  Copyright © 2016 RIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
