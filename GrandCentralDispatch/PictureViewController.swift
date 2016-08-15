12745734e7f54a16a3597bbb4611dacf9975f37c
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-08-15 16:51:36 +0600

    Problem

--- GrandCentralDispatch/PictureViewController.swift
+++ GrandCentralDispatch/PictureViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class PictureViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var imageURL: NSURL?
+    var image: UIImage? {
+        get {return imageView.image}
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.hidden = true
+        }
+    }
+
+    func fetchImage() {
+        imageURL = NSURL(string: "https://wallpaperscraft.ru/image/braziliya_rio_de_zhaneyro_statuya_hrista_spasitelya_brazil_rio_de_janeiro_christ_the_redeemer_vid_sverhu_25377_3840x2160.jpg")
+
+        if let url = imageURL {
+            self.activityIndicator.startAnimating()
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0), {
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue(), {
+                    if imageData != nil {
+                        self.image = UIImage(data: imageData!)
+                    } else {
+                        self.image = nil
+                    }
+                })
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        fetchImage()
     }
 
     override func didReceiveMemoryWarning() {
